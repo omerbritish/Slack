ef676a87bcbf9e4d7047ef5a936c840da5ab3ce4
omerbritish <yekoclass@gmail.com>
2020-05-13 15:56:45 +0100

    update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -5,5 +5,14 @@ public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("ignore me bro");
 
+        String s= "a";
+        if(s.contains("a")){
+            System.out.println("contains");
+        }else{
+            System.out.println("not");
+        }
+
+
+
     }
 }
